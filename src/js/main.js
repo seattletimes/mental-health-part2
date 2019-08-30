e9906f373af10ff8ccbc62cc8563d8406e6e5249
emilymeng <eeng@seattletimes.com>
2019-08-29 18:25:41 -0700

    init from part1

---
+++ src/js/main.js
@@ -0,0 +1,19 @@
+require("./lib/ads");
+var track = require("./lib/tracking");
+var paywall = require("./lib/paywall");
+setTimeout(() => paywall(11086587), 5000);
+
+
+require("component-responsive-frame/child");
+require("./bc-preroll");
+
+// var promise = document.querySelector('video').play();
+
+// if (promise !== undefined) {
+//   promise.then(_ => {
+//     // Autoplay started!
+//   }).catch(error => {
+//     // Autoplay was prevented.
+//     // Show a "Play" button so that user can start playback.
+//   });
+// };
